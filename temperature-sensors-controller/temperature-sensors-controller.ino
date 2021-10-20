677011c4d4c3b535d16e4baaf09a5cbf486669d4
Krzysztof Bogucki <krzysztof.bogucki@asseco.pl>
2021-10-20 15:44:18 +0200

    add web server and sample page

--- temperature-sensors-controller/temperature-sensors-controller.ino
+++ temperature-sensors-controller/temperature-sensors-controller.ino
@@ -1,6 +1,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 #define APP_NAME "CB Sensors Controller"
 #define APP_VERSION "v. 0.0.1"
@@ -9,12 +10,14 @@
 #define WIFI_SSID "WLAN-CENTER"
 #define WIFI_PASSWORD "Tq9h39DjigFgREYH"
 
-
 int sensors_count = 0;
-
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+
+String content;
+ESP8266WebServer server(80);
+
 
 void wifi_connect()
 {
@@ -31,6 +34,8 @@ void wifi_connect()
     console_println("");
     console_print("Connected with IP: " + local_ip);
     console_println("");
+
+    web_server();
 }
 
 void init_sensors()
@@ -79,22 +84,6 @@ void read_sensors()
    
 }
 
-void setup() {
-  
-  Serial.begin(115200);
-  
-  console_println("");
-  console_println("");
-  
-  console_println("=======================================");
-  console_println("     " + String(APP_NAME) + " " + String(APP_VERSION));
-  console_println("=======================================");
-  
-  init_sensors();
-  wifi_connect();
-
-}
-
 void console_print(String a)
 {
   Serial.print(a);
@@ -114,7 +103,48 @@ void print_address(DeviceAddress deviceAddress)
   }
 }
 
+void web_server()
+{
+  
+  server.begin();
+  
+  console_print("idzie content 12");
+  
+  server.on("/", []()
+  {
+    IPAddress ip = WiFi.localIP();
+   
+    content = "<!DOCTYPE HTML>\r\n";
+    content += "<html><center><h1>Hostname: </h1><p></p><h2>IP: ";
+    
+    content += "</h2><p></p><h3>";
+    
+    content += "</h3><p></p><form method='get' action='cleareeprom'><input type='submit' value='Clear EEPROM'>";
+    content += "</center></html>";
+    console_print("idzie content");
+    server.send(200, "text/html", content);        
+  });
+}
+
+void setup() {
+  
+  Serial.begin(115200);
+  
+  console_println("");
+  console_println("");
+  
+  console_println("=======================================");
+  console_println("     " + String(APP_NAME) + " " + String(APP_VERSION));
+  console_println("=======================================");
+
+  init_sensors();
+  wifi_connect();
+
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-  read_sensors();
+  //read_sensors();
+
+  server.handleClient();
 }
